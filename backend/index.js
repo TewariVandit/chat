174b9f474044d2e758c246e8a98bc4b4706fa1c5
vandit tewari <vandittewari16@gmail.com>
2024-09-03 21:10:59 +0530

    nect

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(express.static(path.join
-    (__dirname, "frontend/dir")
+    (__dirname, "frontend/build")
 ))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dir", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 })
 
 const corsOption = {
