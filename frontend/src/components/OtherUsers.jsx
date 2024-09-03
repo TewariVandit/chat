87c271e516607064324334d07413316e05049566
vandit tewari <vandittewari16@gmail.com>
2024-09-03 20:47:26 +0530

    new

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -5,23 +5,19 @@ import {useSelector} from "react-redux";
 
 
 const OtherUsers = () => {
-    // my custom hook
     useGetOtherUsers();
-    const {otherUsers} = useSelector(store=>store.user);
-    if (!otherUsers) return; // early return in react
-     
+    const { otherUsers } = useSelector(store => store.user);
+
+    if (!otherUsers || !Array.isArray(otherUsers)) return null; // Ensure early return if not an array
+
     return (
         <div className='overflow-auto flex-1'>
-            {
-                otherUsers?.map((user)=>{
-                    return (
-                        <OtherUser key={user._id} user={user}/>
-                    )
-                })
-            }
-            
+            {otherUsers.map((user) => (
+                <OtherUser key={user._id} user={user} />
+            ))}
         </div>
-    )
-}
+    );
+};
+
 
 export default OtherUsers
