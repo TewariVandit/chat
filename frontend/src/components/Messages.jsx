ff4fa7ca5b0897d6d22091e26903694bca1dfe9a
vandit tewari <vandittewari16@gmail.com>
2024-09-04 18:45:35 +0530

    sc

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,3 +1,9 @@
+import React from 'react'
+import Message from './Message'
+import useGetMessages from '../hooks/useGetMessages';
+import { useSelector } from "react-redux";
+import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
+
 const Messages = () => {
     useGetMessages();
     useGetRealTimeMessage();
@@ -14,3 +20,5 @@ const Messages = () => {
         </div>
     )
 }
+
+export default Messages
