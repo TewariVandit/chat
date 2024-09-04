8a19ee76e1a739a365822608b62722a014b1fc63
vandit tewari <vandittewari16@gmail.com>
2024-09-04 18:41:13 +0530

    dsf

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BiSearchAlt2 } from "react-icons/bi";
-import { FaBars } from "react-icons/fa"; // Importing the hamburger icon
+import { FaBars } from "react-icons/fa";
 import OtherUsers from './OtherUsers';
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -12,7 +12,7 @@ import { BASE_URL } from '..';
 
 const Sidebar = () => {
     const [search, setSearch] = useState("");
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Default to closed on small screens
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const { otherUsers } = useSelector(store => store.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -47,7 +47,6 @@ const Sidebar = () => {
 
     return (
         <div className="relative w-[20%] bg-white z-50">
-            {/* Hamburger menu button for small screens */}
             <button
                 className="fixed top-4 right-4 z-50 p-2 bg-gray-800 text-white rounded-full md:hidden"
                 onClick={toggleSidebar}
@@ -55,23 +54,22 @@ const Sidebar = () => {
                 <FaBars className="w-6 h-6" />
             </button>
 
-            {/* Sidebar for larger screens and toggleable on small screens */}
-            <div 
-                className={`fixed inset-0 md:relative md:w-64  h-[100%] border-r border-slate-500 p-4 flex flex-col bg-white transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}
+            <div
+                className={`fixed inset-0 md:relative md:w-64 h-[100%] border-r border-slate-500 p-4 flex flex-col bg-white transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}
                 style={{ top: '0' }}
             >
                 <h1 className='text-2xl font-bold mb-4 text-black'>Chats</h1>
-                <form onSubmit={searchSubmitHandler} action="" className='flex items-center gap-2 '>
+                <form onSubmit={searchSubmitHandler} action="" className='flex items-center gap-2'>
                     <input
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
-                        className='input input-bordered rounded-md bg-white' type="text"
+                        className='input input-bordered rounded-md bg-white'
+                        type="text"
                         placeholder='Search...'
                     />
-                    
                 </form>
                 <div className="divider px-3"></div>
-                <OtherUsers />
+                <OtherUsers toggleSidebar={toggleSidebar} />
                 <div className='mt-2'>
                     <button onClick={logoutHandler} className='btn btn-sm bg-[#009688] text-white'>Logout</button>
                 </div>
